98efae2fbc1f8fdc64e7e15e5c251e4eb3acb6bc
martinbenny1090 <martin8086benny@gmail.com>
2020-03-19 18:17:55 +0530

    DTL change

--- sports_world/settings.py
+++ sports_world/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'sports_world.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS':edfuiuf ,
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
