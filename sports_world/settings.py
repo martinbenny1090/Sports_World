15c426991d837b360df27305883ee388f069d8a8
martinbenny1090 <martin8086benny@gmail.com>
2020-03-23 12:47:19 +0530

    not working

--- sports_world/settings.py
+++ sports_world/settings.py
@@ -129,3 +129,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- sports_world/urls.py
+++ sports_world/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('',include('user.urls')),
     path('owner',include('owner.urls')),
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls)
+]+ static(settings.MEDIA_URL, documents_root=settings.MEDIA_ROOT)
 
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
