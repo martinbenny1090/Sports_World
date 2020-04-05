af5cce3769abfffc2b6b1abf3b3df94f5c5c8db6
martinbenny1090 <martin8086benny@gmail.com>
2020-04-05 21:53:36 +0530

    owner home page editing

--- sports_world/settings.py
+++ sports_world/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accounts',
     'user',
+    'owner',
 ]
 
 MIDDLEWARE = [
--- sports_world/urls.py
+++ sports_world/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('',include('user.urls')),
     path('accounts/',include('accounts.urls')),
+    path('owner/',include('owner.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
